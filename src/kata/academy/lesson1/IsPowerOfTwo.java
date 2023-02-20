9dca411622726975140b756c757c020243ae4c02
Stas_Litvinov <staslitvinovqa@mail.com>
2023-02-21 00:55:14 +0400

    Added task 2.2.8. «Метод isPowerOfTwo»

---
+++ src/kata/academy/lesson1/IsPowerOfTwo.java
@@ -0,0 +1,14 @@
+package kata.academy.lesson1;
+
+// 2.2.8. Кодовая задача «Метод isPowerOfTwo»
+
+class IsPowerOfTwo {
+    public static void main(String[] args) {
+
+        System.out.println(isPowerOfTwo(-2));
+    }
+
+    public static boolean isPowerOfTwo(int value) {
+        return Math.abs(value % 2) == 0;
+    }
+}
