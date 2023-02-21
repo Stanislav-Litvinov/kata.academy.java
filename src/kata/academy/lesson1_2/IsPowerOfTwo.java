7225156eceda5b0e49a294fb8a07a4795caa7c71
Stas_Litvinov <staslitvinovqa@mail.com>
2023-02-21 21:41:49 +0400

    Updated the task IsPowerOfTwo. Adde few examples

--- src/kata/academy/lesson1_2/IsPowerOfTwo.java
+++ src/kata/academy/lesson1_2/IsPowerOfTwo.java
@@ -4,15 +4,14 @@ package kata.academy.lesson1_2;
 
 class IsPowerOfTwo {
     public static void main(String[] args) {
-        System.out.println("First example 0 = false: " + isPowerOfTwo(0));
-        System.out.println("Second example 1 = true: " + isPowerOfTwo(1));
-        System.out.println("Third example -2 = true: " + isPowerOfTwo(-2));
+        System.out.println("Input example 0 = false: " + isPowerOfTwo(0));
+        System.out.println("Input example 1 = true: " + isPowerOfTwo(1));
+        System.out.println("Input example -2 = true: " + isPowerOfTwo(-2));
+        System.out.println("Input example 22 = false: " + isPowerOfTwo(22));
     }
 
     public static boolean isPowerOfTwo(int value) {
-        if (value != 0) {
-            return Math.abs(value % 2) == 0 || value == 1;
-        }
-        return false;
+        value = Math.abs(value);
+        return (value != 0) && ((value & (value - 1)) == 0);
     }
 }
