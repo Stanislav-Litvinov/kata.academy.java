fb03b3d6c3045a948bbcc0284cc103a01b35a3bf
Stas_Litvinov <staslitvinovqa@mail.com>
2023-02-27 22:41:01 +0400

    Added task PrintArray

---
+++ src/kata/academy/lesson4/PrintArray.java
@@ -0,0 +1,22 @@
+package kata.academy.lesson4;
+
+// 4.1.3. Кодовая задача «Печать массива в консоль»
+
+class PrintArray {
+    public static void main(String[] args) {
+        printArray(new int[]{});
+        printArray(new int[]{1, 2, 3});
+        printArray(new int[]{1, 2, 3, 4, 5, 5, 6, 7, 7});
+    }
+
+    public static void printArray(int[] numbers) {
+        System.out.print("[");
+        String separator = "";
+        for (int number : numbers) {
+            System.out.print(separator);
+            System.out.print(number);
+            separator = ",";
+        }
+        System.out.println("]");
+    }
+}
