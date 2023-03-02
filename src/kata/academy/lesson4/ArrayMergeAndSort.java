fde863ac4499eee52e7c4d79397f834d944c51ff
Stas_Litvinov <staslitvinovqa@mail.com>
2023-03-02 17:49:37 +0400

    Added task ArrayMergeAndSort.java

---
+++ src/kata/academy/lesson4/ArrayMergeAndSort.java
@@ -0,0 +1,21 @@
+package kata.academy.lesson4;
+
+// 4.1.11.  Кодовая задача «Слияние массивов c помощью Arrays»
+
+import java.util.Arrays;
+
+class ArrayMergeAndSort {
+    public static void main(String[] args) {
+        mergeAndSort(new int[]{1, 3, 7, 5}, new int[]{-8, 4, 2, 4});
+        mergeAndSort(new int[]{}, new int[]{1, 4, 3});
+        mergeAndSort(new int[]{}, new int[]{});
+    }
+
+    public static void mergeAndSort(int[] firstArray, int[] secondArray) {
+        int[] newArr = new int[firstArray.length + secondArray.length];
+        System.arraycopy(firstArray, 0, newArr, 0, firstArray.length);
+        System.arraycopy(secondArray, 0, newArr, firstArray.length, secondArray.length);
+        Arrays.sort(newArr);
+        System.out.println(Arrays.toString(newArr));
+    }
+}
